8e5b6ca037b77c88535ba689ab53448ae69a34c2
yogeshmlv <yttmlv@gmail.com>
2023-02-17 04:19:01 +0530

    To do list

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Today Task */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+     
+
+        <View style={styles.items}>
+          {/* This is Where the task will go ! */}
+          <Task text={'React Native'}/>
+        <Task text={'Raect Js'}/>
+        <Task text ={'JavaScript'}/>
+        <Task text ={'Dealing With Task '}/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +24,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: "#E8EAED",
+    backgroundColor:"pink"
+  },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+  },
+  items: {
+    marginTop:50,
   },
 });
